527ed942693209b3c023b7cfc2354e4a0fdeb3c0
vasvasa <vasilev.vasiliy.a@gmail.com>
2022-11-29 22:43:23 +0800

    added bundles and base entity

---
+++ src/Entity/News.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\NewsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NewsRepository::class)]
+#[ApiResource]
+class News
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $text = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $publicationDateTime = null;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $rating = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $source = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(?string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getPublicationDateTime(): ?\DateTimeInterface
+    {
+        return $this->publicationDateTime;
+    }
+
+    public function setPublicationDateTime(\DateTimeInterface $publicationDateTime): self
+    {
+        $this->publicationDateTime = $publicationDateTime;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(?int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getSource(): ?string
+    {
+        return $this->source;
+    }
+
+    public function setSource(string $source): self
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+}
