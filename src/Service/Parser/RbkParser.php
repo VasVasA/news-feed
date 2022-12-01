a256e1850ecb7c4c5c44ed7048cae436d3d3ec68
vasvasa <vasilev.vasiliy.a@gmail.com>
2022-12-01 08:40:18 +0800

    added parser

---
+++ src/Service/Parser/RbkParser.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Service\Parser;
+
+class RbkParser implements ParserInterface
+{
+
+    public function __construct(public string $sourceUrl)
+    {
+    }
+
+    public function parse(): array
+    {
+        
+    }
+
+}
