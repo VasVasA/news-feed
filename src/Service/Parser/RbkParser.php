5f7dcc072ef2722f9259478e96d52262f36b6e65
vasvasa <vasilev.vasiliy.a@gmail.com>
2022-12-04 21:56:59 +0800

    WIP add parsing

--- src/Service/Parser/RbkParser.php
+++ src/Service/Parser/RbkParser.php
@@ -4,19 +4,32 @@ namespace App\Service\Parser;
 
 use App\Service\WebApi\WebApiInterface;
 use DOMDocument;
+use DOMXPath;
 
 class RbkParser implements ParserInterface
 {
 
-    public function __construct(private readonly string $sourceUrl, private readonly WebApiInterface $webApi)
-    {
+    public function __construct(
+        private readonly string $sourceUrl,
+        private readonly WebApiInterface $webApi,
+        private readonly string $classname = "news-feed__item"
+    ) {
     }
 
     public function parse(): array
     {
         $page = $this->webApi->sendRequest($this->sourceUrl);
         $dom = new DOMDocument();
-        $dom->loadHTML($page);
+        $internalErrors = libxml_use_internal_errors(true);
+        $dom->loadHTML(html_entity_decode($page));
+        libxml_use_internal_errors($internalErrors);
+        $finder = new DomXPath($dom);
+        $expression = './/div[contains(concat(" ", normalize-space(@class), " "), " '.$this->classname.' ")]';
+        $itemList = $finder->evaluate($expression);
+        foreach ($itemList as $item) {
+
+        }
+
         return json_encode($page);
     }
 
--- src/Service/WebApi/WebApi.php
+++ src/Service/WebApi/WebApi.php
@@ -10,6 +10,16 @@ use Exception;
 class WebApi implements WebApiInterface
 {
     /**
+     * @var string
+     */
+    private string $cookie = "";
+
+    /**
+     * @var string
+     */
+    private string $user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0';
+
+    /**
      * Сделать запрос к ресурсу
      *
      * @param string $url
@@ -25,11 +35,16 @@ class WebApi implements WebApiInterface
             $curlDescriptor, [
                 CURLOPT_URL => $url,
                 CURLOPT_POST => $isPost,
-                CURLOPT_POSTFIELDS => $postFields,
                 CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_FOLLOWLOCATION => true
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_COOKIEJAR => $this->cookie,
+                CURLOPT_COOKIEFILE => $this->cookie,
+                CURLOPT_USERAGENT => $this->user_agent
             ]
         );
+        if ($isPost) {
+            curl_setopt($curlDescriptor, CURLOPT_POSTFIELDS, $postFields);
+        }
         $curlResponse = curl_exec($curlDescriptor);
         if (curl_errno($curlDescriptor)) {
             $errorMessage = curl_error($curlDescriptor);
